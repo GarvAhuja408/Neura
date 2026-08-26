eba5ecd6099e7b9e114edd7b63648fbb78588bf4
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-26 14:54:29 +0530

    test route

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
+import chatRoutes from "./routes/chat.js";
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
@@ -12,6 +13,8 @@ const PORT = 8080;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api",chatRoutes);
 
 mongoose.connect(process.env.MONGO_URL)
     .then(() => {
