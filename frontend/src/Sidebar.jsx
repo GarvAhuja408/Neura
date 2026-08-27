913164638e5581e2dfc72cf514ad27173b6557c7
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-27 15:35:11 +0530

    Sidebar setup

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,9 +2,24 @@ import "./Sidebar.css";
 
 function Sidebar(){
     return (
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+            
+            <button>
+                <img src="src/assets/blacklogo.png" alt="gpt logo"></img>
+                <i className="fa-solid fa-pen-to-square"></i>
+            </button>
+
+            <ul className="history">
+                <li>history 1</li>
+                <li>history 1</li>
+                <li>history 1</li>
+                <li>history 1</li>
+            </ul>
+
+            <div className="sign">
+                <p>By Garv Ahuja</p>
+            </div>
+        </section>
     )
 }
 
