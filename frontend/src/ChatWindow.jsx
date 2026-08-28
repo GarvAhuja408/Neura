afca53b094accfa7ba26a98277b28233becb3633
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-28 14:38:41 +0530

    Worked on chat window

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,9 +1,35 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow(){
     return (
-        <div>
-            ChatWindow
+        <div className="chatwindow">
+
+            <div className="navbar">
+                <span>Neura <i class="fa-solid fa-chevron-down"></i></span>
+                <div className="userIconDiv">
+                    <span><i class="fa-solid fa-user"></i></span>
+                </div>
+            </div>
+
+            <Chat/>
+
+            <div className="chatInput">
+
+                <div className="userInput">
+
+                    <input placeholder="Ask Anything">
+                    </input>
+                    <div id="submit">
+                        <i class="fa-solid fa-paper-plane"></i>
+                    </div>
+                </div>
+                <p className="info">
+                    AI can make mistakes. Check important info. See cookie preferences
+                </p>
+
+            </div>
+
         </div>
     )
 }
