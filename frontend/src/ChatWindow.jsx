1c93ee3e1a470f18fc05d4043ac7c4af289d6878
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-28 15:54:03 +0530

    Worked on backend and frontend for response during DOM events

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,7 +1,37 @@
 import "./ChatWindow.css";
 import Chat from "./Chat.jsx";
+import { MyContext } from "./MyContext.jsx";
+import { useContext } from "react";
+
 
 function ChatWindow(){
+
+    const {prompt,setPrompt,reply,setReply,currThreadId}=useContext(MyContext);
+
+    const getReply = async()=>{
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                message:prompt,
+                threadId:currThreadId
+            })
+        }
+
+    try{
+        const response = await fetch("http://localhost:8080/api/chat",options);
+        const res = await response.json();
+        setReply(res.reply)
+        console.log(res.reply);
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+    
+
     return (
         <div className="chatwindow">
 
@@ -18,10 +48,11 @@ function ChatWindow(){
 
                 <div className="inputBox">
 
-                    <input placeholder="Ask Anything">
+                    <input placeholder="Ask Anything" value={prompt} onChange={(e)=> setPrompt(e.target.value)} onKeyDown={(e)=>e.key ==='Enter' ? getReply():' '}>
+                        
                     </input>
                     &nbsp;&nbsp;
-                    <div id="submit">
+                    <div id="submit" onClick={getReply}>
                         <i class="fa-solid fa-paper-plane"></i>
                     </div>
                 </div>
