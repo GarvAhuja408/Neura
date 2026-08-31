a83e65e81843d2d730ce66fdd950a7500e5e36ce
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-08-31 15:24:02 +0530

    used setinterval to display chats in writing format along useState and usEffect hooks

--- frontend/src/Chat.jsx
+++ frontend/src/Chat.jsx
@@ -1,5 +1,5 @@
 import "./Chat.css";
-import { useContext } from "react";
+import { useContext,useState,useEffect} from "react";
 import { MyContext } from "./MyContext";
 
 import Markdown from 'react-markdown'
@@ -9,14 +9,33 @@ import "highlight.js/styles/github-dark.css";
 
 function Chat(){
 
-    const{newChat,prevChats} = useContext(MyContext);
+    const{newChat,prevChats,reply} = useContext(MyContext);
+    const [latestReply,setLatestReply]=useState(null);
+
+    useEffect( ()=>{
+        if(!prevChats?.length)return ;
+
+        const content=reply.split(" ");
+        let idx=0;
+
+        const interval=setInterval ( ()=>{
+            setLatestReply(content.slice(0,idx+1).join(" "));
+
+            idx++;
+
+            if(idx>=content.length)clearInterval(interval);
+        },40);
+
+        return ()=> clearInterval(interval);
+
+    },[prevChats,reply])
 
     return (
         <>
         {newChat && <h2>Start a chat</h2>}
         <div className="chats">
             {
-                prevChats?.map((chat,idx)=>
+                prevChats?.slice(0,-1).map((chat,idx)=>
                     <div className={chat.role==="user"? "userDiv" :"gptDiv"} key={idx}>
                         {
                             chat.role==="user"? 
@@ -28,7 +47,14 @@ function Chat(){
                     </div>
                 )
             }
-            
+            {
+                prevChats.length>0&&latestReply!=null&&
+                <div className="gptDiv" key={"typing"}>
+                    <Markdown rehypePlugins={[rehypeHighlight]}>
+                                        {latestReply}
+                                    </Markdown>
+                </div>
+            }   
         </div>
         </>
     )
