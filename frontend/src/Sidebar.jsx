d8309424e44d2860019312481e079be909d1d79a
GarvAhuja408 <garv.ahuja0309@gmail.com>
2026-09-01 15:04:00 +0530

    Added functionality for displaying threads as per title to make thread dynamic

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -1,6 +1,30 @@
 import "./Sidebar.css";
+import { useContext,useEffect } from "react";
+import { MyContext } from "./MyContext";
+
 
 function Sidebar(){
+
+    const {allThreads,setAllThreads,currThreadId}=useContext(MyContext);
+
+    const getAllThreads= async()=>{
+
+        try{
+            const response = await fetch("http://localhost:8080/api/thread");
+            const res = await response.json();
+
+            const filteredData= res.map( (thread) => ({threadId: thread.threadId, title: thread.title}));
+            console.log(filteredData);
+            setAllThreads(filteredData);
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    useEffect( ()=>{
+        getAllThreads();
+    },[currThreadId]);
+
     return (
         <section className="sidebar">
             
@@ -10,10 +34,11 @@ function Sidebar(){
             </button>
 
             <ul className="history">
-                <li>Thread 1</li>
-                <li>Thread 2</li>
-                <li>Thread 3</li>
-                <li>Thread 4</li>
+                {
+                    allThreads?.map( (thread,idx)=>(
+                        <li key={idx}>{thread.title}</li>
+                    ))
+                }
             </ul>
 
             <div className="sign">
